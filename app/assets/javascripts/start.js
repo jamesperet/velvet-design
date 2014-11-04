695767eb81c57b6521689c3b4bdf9fdd2ffb90e5
jamesperet <james.peret@gmail.com>
2014-11-04 19:41:19 -0200

    Front page layout

--- app/assets/javascripts/start.js
+++ app/assets/javascripts/start.js
@@ -1,7 +1,8 @@
 // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
 $( document ).ready(function() {
-	var mainbottom = $('#myCarousel').offset().top + $('#myCarousel').height();
-
+	var mainbottom = $('#myCarousel').offset().top + ($('#myCarousel').height()*2)-10;
+	var about_bottom = $('#post').offset().top +  $('#post-image').height() - (screen.height - $('#post-image').height() + 152);
+	var post_bottom = $('.feature-post-text').offset().top +  $('.feature-post-text').height() - (screen.height - $('.feature-post-text').height() + 232);
 
 	// on scroll, 
 	$(window).on('scroll',function(){
@@ -15,5 +16,34 @@ $( document ).ready(function() {
 	   }
 
 	});
+	
+	// on scroll, 
+	$(window).on('scroll',function(){
+
+	    // we round here to reduce a little workload
+	    stop = Math.round($(window).scrollTop());
+	    if (stop > about_bottom) {
+	        $('#post-image').addClass('affix top-affix ');
+	    } else {
+	        $('#post-image').removeClass('affix top-affix ');
+	   }
+
+	});
+	
+	// on scroll, 
+	$(window).on('scroll',function(){
+
+	    // we round here to reduce a little workload
+	    stop = Math.round($(window).scrollTop());
+	    if (stop > post_bottom && stop > about_bottom) {
+	        $('#post-image').removeClass('affix top-affix ');
+		   var textsize = $('.feature-post-text').height() - $('#post-image').height()/2 - 80
+		   $('#post-image').css('margin-top', textsize);
+	    } else {
+	    		$('#post-image').css('margin-top', '0');
+	    }
+
+	});
+	
 
 });
--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -4,6 +4,7 @@ class StartController < ApplicationController
   
   def index
     @blog_posts = BlogPost.order('created_at DESC').take(4)
+    @last_post = BlogPost.last
   end
 
 end
